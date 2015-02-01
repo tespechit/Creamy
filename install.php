14ac0e3ba086e8fc8f0e062927306bcf69ad24da
DigitalLeaves <contact@digitalleaves.com>
2015-02-01 22:38:46 +0100

    Fixed missing translation for “Create” Button.

--- install.php
+++ install.php
@@ -342,7 +342,7 @@ define('CRM_SECURITY_TOKEN', '$crmSecurityCode');
                 	</div>
                 </div>
                 <div class="footer">                                                               
-                    <button type="submit" name="submit_step3" id="submit_step3" class="btn bg-light-blue btn-block">Crear</button>  
+                    <button type="submit" name="submit_step3" id="submit_step3" class="btn bg-light-blue btn-block"><?php $this->lh->translateText("create"); ?></button>  
                 </div>
             </form>
 
